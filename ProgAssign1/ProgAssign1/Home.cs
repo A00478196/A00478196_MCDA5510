42dceb44382bc461f7faa1c97e41a5d1df878d97
A00478196 <kritika.koirala@smu.ca>
2023-10-25 20:55:35 -0300

    added the global try catch function

--- ProgAssign1/ProgAssign1/Home.cs
+++ ProgAssign1/ProgAssign1/Home.cs
@@ -1,6 +1,7 @@
 ﻿using CsvHelper;
 using Serilog;
 using System.Diagnostics;
+using System.Security;
 
 namespace ProgAssign1
 {
@@ -11,40 +12,73 @@ namespace ProgAssign1
         public static void Main (String[] args)
         {
 
-            // Initial configuration to write log to a log.txt file
-            
+            try
+            {
+
+
+
+                // Initial configuration to write log to a log.txt file
+
                 Log.Logger = new LoggerConfiguration()
             .WriteTo.File("..\\..\\..\\logs\\log.txt") // Specify the file path here
             .CreateLogger();
-            
 
-            Log.Information("---- The Program started ----");
 
-            // Start the timer when the program is run
-            var timer = new Stopwatch();
-            timer.Start();
+                Log.Information("---- The Program started ----");
+
+                // Start the timer when the program is run
+                var timer = new Stopwatch();
+                timer.Start();
+
+
+                List<string> fileEntries = new List<string>();
+
+
+                // Initialize the classes to read thru the directories and handle the csv files
+                DirWalker dirWalk = new DirWalker();
+                CSVHandler csvHandler = new CSVHandler();
+
+
+                //store the read directories in a list called fileEntries
+                fileEntries = dirWalk.Walker();
+
+                //pass the list of files to csvhandler class to write to the output along with the timer
+                csvHandler.readWrite(fileEntries, timer);
+
+
+
+                Log.Information("---- The program has ended... ----");
+                Log.Information("");
+                Log.CloseAndFlush();
+            }
+            catch (IOException e)
+            {
+                Log.Information("An IOE occurred: " + e.Message);
+            }
+            catch (CsvHelperException e)
+            {
+                Log.Information("A CsvHelperException occurred: " + e.Message);
+
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Log.Information("A out of range exception occured: " + e.Message);
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Log.Information("Unauthorized exception occurred: " + e.Message);
+            }
+            catch (ArgumentNullException e)
+            {
+                Log.Information("ArgumentNullException occurred: " + e.Message);
+            }
+            catch (SecurityException e)
+            {
+                Log.Information("ArgumentNullException occurred: " + e.Message);
+            }
 
           
-            List<string> fileEntries = new List<string>();
-            
-
-            // Initialize the classes to read thru the directories and handle the csv files
-            DirWalker dirWalk = new DirWalker();
-            CSVHandler csvHandler = new CSVHandler();
-
-
-            //store the read directories in a list called fileEntries
-            fileEntries = dirWalk.Walker();
-
-            //pass the list of files to csvhandler class to write to the output along with the timer
-            csvHandler.readWrite(fileEntries, timer);
-
-
-
-            Log.Information("---- The program has ended... ----");
-            Log.Information("");
-            Log.CloseAndFlush();
 
         }
-}
+    }
 }
